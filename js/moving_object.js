8be5157976aec36a8012297c192abab74f76a0b3
App Academy Student <esop+matt@appacademy.io>
2014-06-11 12:10:06 -0400

    Create MovingObject methods

--- js/moving_object.js
+++ js/moving_object.js
@@ -9,15 +9,34 @@
   };
   
   MovingObject.prototype.move = function() {
-    //do physics here
+    this.pos[0] += this.vel[0];
+    this.pos[1] += this.vel[1];
   };
   
   MovingObject.prototype.draw = function(ctx) {
-    //draw a circle with correct radius around pos
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+
+    ctx.arc(
+      this.pos[0],
+      this.pos[1],
+      this.radius,
+      0,
+      2 * Math.PI,
+      false
+    );
+
+    ctx.fill();
   };
+  
   
   MovingObject.prototype.isCollidedWith = function(otherObject) {
     //compute the distance between the two centers fo the objects.
+    var xDiff = this.pos[0] - otherObject.pos[0];
+    var yDiff = this.pos[1] - otherObject.pos[1];
+    var distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    
+    return ((this.radius + otherObject.radius) > distance);
   };
   
 })(this)
